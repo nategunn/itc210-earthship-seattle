a93fecc45b7c598c4c2a7c9b4f4e07705e3fc9b7
Nate Gunn <nategunn2000@gmail.com>
2014-10-23 14:52:42 -0700

    Add bare bones template

---
+++ theme-template/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	
+</head>
+
+<body>
+	<header id="masthead" >
+		<h1>header</h1>
+	</header><!-- #masthead -->
+
+	<div id="main" class="site-main">
