870966de602627499b3419ef0ee7fc1b71ce97f8
Nate Gunn <nategunn2000@gmail.com>
2014-11-03 17:35:39 -0800

    add padding to .post-box and wrap .postmetadata in h4

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
     <?php while (have_posts()) : the_post(); ?>
     	<div class="post-box">
     	<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
-        <p class="postmetadata">
+        <h4 class="postmetadata">
             <span class="date">Posted on <?php the_time('F jS, Y') ?></span> in 
             <span class="cat"><?php the_category(', ') ?></span>
             <span class="author"><?php _e('By');?> <?php the_author_posts_link(); ?></span>
          	<span class="comments">with <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?></span>
-        </p>
+        </h4>
 		<?php the_content('More &raquo;'); ?>
      	<?php edit_post_link('Edit this entry.', '<p><small>', '</small></p>'); ?>
         </div>
