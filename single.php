80407981d1cf60f7fcf7c5f8e31cd79ae09eae9e
Nate Gunn <nategunn2000@gmail.com>
2014-11-05 15:05:25 -0800

    add functional single.php

--- single.php
+++ single.php
@@ -1,10 +1,30 @@
 <?php get_header(); ?>
 
-	<div id="primary">
-		<h1>Single</h1>
-	</div><!-- #primary -->
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div id="content" class="single">
+    <strong class="breadcrumb"><?php if (function_exists('bcn_display')) { bcn_display(); } ?></strong>
+        <h2><?php the_title(); ?></h2>     
+    <p class="postmetadata">
+    	<span class="date">Posted on <?php the_time('F jS, Y') ?></span> in 
+		<span class="cat"><?php the_category(', ') ?></span>
+		<span class="author"><?php _e('By');?> <?php the_author_posts_link(); ?></span>
+    </p>
+    <?php the_content('More &raquo;'); ?>
+    
+		<nav class="post-nav">            
+			<div class="alignleft"><?php previous_post_link('&laquo; %link', '%title', true) ?></div>
+			<div class="alignright"><?php next_post_link('%link &raquo;', '%title', true) ?></div>
+		</nav>
+    
+    <?php edit_post_link('Edit this entry.', '<p><small>', '</small></p>'); ?>
+	<?php comments_template(); ?>
+    <?php endwhile; else: ?>
+    <p>Sorry, no posts matched your criteria.</p>
+    <?php endif; ?>
+</div><!-- end content -->
 
-<?php
-
-get_sidebar();
-get_footer();
+<div>
+        <?php get_sidebar();?>
+</div>
+<div id='delimiter'></div>
+<?php get_footer();
