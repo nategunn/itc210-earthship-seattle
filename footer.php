86d5945166dcab534d9588d2ced8b7b18b1312a4
cluetrain <greenstrat67@gmail.com>
2014-11-20 20:50:44 -0800

    change class names to footer columns

--- footer.php
+++ footer.php
@@ -3,6 +3,60 @@
 		</div><!-- #middle -->
 
 		<footer id="site-footer" >
+		
+			<div class="footer-nav">
+			<div class="c1">			
+				<p class="bt_1"><a href="javascript:void(0)">GET INVOLVED</a></p>
+				<p class="bt_2"><a href="javascript:void(0)">NEWSLETTER</a></p>
+				<p class="bt_3"><a href="javascript:void(0)">CONTRIBUTE</a></p>
+				
+			</div>
+			<div class="c2">
+				<ul>
+					<li><a href="javascript:void(0)">ABOUT</a></li>
+					<li><a href="javascript:void(0)">US</a></li>
+					<li><a href="javascript:void(0)">PARENT</a></li>
+					<li><a href="javascript:void(0)">EDUCATION</a></li>
+					<li><a href="javascript:void(0)">OUR EVENTS</a></li>
+					<li><a href="javascript:void(0)">OUR PROJECTS</a></li>
+				</ul>
+			</div>
+			<div class="c2">
+				<ul>
+					<li><a href="javascript:void(0)">CALENDAR</a></li>
+				</ul>
+			</div>
+			<div class="c2">
+				<ul>
+					<li><a href="javascript:void(0)">FORUM</a></li>
+				</ul>
+			</div>
+			<div class="c2">
+				<ul>
+					<li><a href="javascript:void(0)">BLOG</a></li>
+					<li><a href="javascript:void(0)">EVENTS</a></li>
+					<li><a href="javascript:void(0)">PROJECTS</a></li>
+				</ul>
+			</div>
+			<div class="c2">
+				<ul>
+					<li><a href="javascript:void(0)">RESOURCES</a></li>
+					<li><a href="javascript:void(0)">LAND</a></li>
+					<li><a href="javascript:void(0)">PERMITS</a></li>
+					<li><a href="javascript:void(0)">LEGAL</a></li>
+				</ul>
+			</div>
+			<div class="c2">
+				<ul>
+					<li><a href="javascript:void(0)">PHOTOS</a></li>
+				</ul>
+			</div>
+			<div class="c2 last">
+				<ul>
+					<li><a href="javascript:void(0)">CONTACT</a></li>
+				</ul>
+			</div>
+			</div><!-- end footer-nav -->		
 			<p><?php print ("&copy; " . date ('Y') . " "); ?>Earthship Seattle &bull; All Rights Reserved &bull; <a href="#top"> TOP OF PAGE </a></p>	
 		</footer>
 	</div><!-- #page -->
