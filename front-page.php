27cebddd2032bbc67834c62736647d018a397ebd
Nate Gunn <nategunn2000@gmail.com>
2014-11-24 10:37:01 -0800

    remove 'title' from front page

--- front-page.php
+++ front-page.php
@@ -22,7 +22,6 @@ Template Name: Front Page
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
         <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-            <h1><?php the_title(); ?></h1>
             <?php the_content(); ?>
         </div>
 
