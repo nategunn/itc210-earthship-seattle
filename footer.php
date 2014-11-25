5af13b16e405d43390661c6b3786c9fa3e841e7d
lorrainekj31 <lorrainekj31@gmail.com>
2014-11-24 20:42:43 -0800

    Comments out closing #page div tag.

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 </div><!-- #middle -->    
 
 <<<<<<< HEAD
-
 		</div><!-- #middle -->
 	<div class="footer-container clear-fix">
 		<footer id="site-footer" >
@@ -61,69 +60,11 @@
 			</div><!-- end footer-nav -->		
 			<p><?php print ("&copy; " . date ('Y') . " "); ?>Earthship Seattle &bull; All Rights Reserved &bull; <a href="#"> TOP OF PAGE </a></p>	
 		</footer>
-	</div><!-- #page -->
+	<!--</div>--><!-- #page -->
 </div><!-- close footer-container -->
         <?php wp_footer(); ?>
         <script src="//localhost:35729/livereload.js"></script>
 =======
-<div class="footer-container clear-fix">
-<footer id="site-footer" class="wrapper940">
-	<div class="footer-nav clear-fix">
-		<div class="c1">			
-			<p class="pseudo-button"><a href="#">GET INVOLVED</a></p>
-			<p class="pseudo-button"><a href="#">NEWSLETTER</a></p>
-			<p class="pseudo-button"><a href="#">CONTRIBUTE</a></p>				
-		</div>
-		<div class="c2">
-			<ul>
-				<li><a href="#">ABOUT</a></li>
-				<li><a href="#">US</a></li>
-				<li><a href="#">PARENT</a></li>
-				<li><a href="#">EDUCATION</a></li>
-				<li><a href="#">OUR EVENTS</a></li>
-				<li><a href="#">OUR PROJECTS</a></li>
-			</ul>
-		</div>
-		<div class="c2">
-			<ul>
-				<li><a href="#">CALENDAR</a></li>
-			</ul>
-		</div>
-		<div class="c2">
-			<ul>
-				<li><a href="#">FORUM</a></li>
-			</ul>
-		</div>
-		<div class="c2">
-			<ul>
-				<li><a href="#">BLOG</a></li>
-				<li><a href="#">EVENTS</a></li>
-				<li><a href="#">PROJECTS</a></li>
-			</ul>
-		</div>
-		<div class="c2">
-			<ul>
-				<li><a href="#">RESOURCES</a></li>
-				<li><a href="#">LAND</a></li>
-				<li><a href="#">PERMITS</a></li>
-				<li><a href="#">LEGAL</a></li>
-			</ul>
-		</div>
-		<div class="c2">
-			<ul>
-				<li><a href="#">PHOTOS</a></li>
-			</ul>
-		</div>
-		<div class="c2 last">
-			<ul>
-				<li><a href="#">CONTACT</a></li>
-			</ul>
-		</div>
-	</div><!-- end footer-nav -->				
-	<p id="copy-right-info" class="delimiter"><?php print ("&copy; " . date ('Y') . " "); ?>Earthship Seattle &bull; All Rights Reserved &bull; <a href="#top"> TOP OF PAGE </a></p>	
-</footer>
-</div> <!-- end footer-container -->
-<script src="//localhost:35729/livereload.js"></script>
 >>>>>>> origin/master
 </body>
 </html>
