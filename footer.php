d84e0a5f514da8e55f5def2a89724eaf20746155
lorrainekj31 <lorrainekj31@gmail.com>
2014-11-24 16:26:29 -0800

    changes to footer psuedo-button styles.
    attempt to commit

--- footer.php
+++ footer.php
@@ -2,65 +2,61 @@
 
 <div id="footer-wide">
 <footer id="site-footer" class="wrapper940">
-
-	<div class="footer-nav">
-	<div class="c1">			
-		<p class="bt_1"><a href="javascript:void(0)">GET INVOLVED</a></p>
-		<p class="bt_2"><a href="javascript:void(0)">NEWSLETTER</a></p>
-		<p class="bt_3"><a href="javascript:void(0)">CONTRIBUTE</a></p>
-		
-	</div>
-	<div class="c2">
-		<ul>
-			<li><a href="javascript:void(0)">ABOUT</a></li>
-			<li><a href="javascript:void(0)">US</a></li>
-			<li><a href="javascript:void(0)">PARENT</a></li>
-			<li><a href="javascript:void(0)">EDUCATION</a></li>
-			<li><a href="javascript:void(0)">OUR EVENTS</a></li>
-			<li><a href="javascript:void(0)">OUR PROJECTS</a></li>
-		</ul>
-	</div>
-	<div class="c2">
-		<ul>
-			<li><a href="javascript:void(0)">CALENDAR</a></li>
-		</ul>
-	</div>
-	<div class="c2">
-		<ul>
-			<li><a href="javascript:void(0)">FORUM</a></li>
-		</ul>
-	</div>
-	<div class="c2">
-		<ul>
-			<li><a href="javascript:void(0)">BLOG</a></li>
-			<li><a href="javascript:void(0)">EVENTS</a></li>
-			<li><a href="javascript:void(0)">PROJECTS</a></li>
-		</ul>
-	</div>
-	<div class="c2">
-		<ul>
-			<li><a href="javascript:void(0)">RESOURCES</a></li>
-			<li><a href="javascript:void(0)">LAND</a></li>
-			<li><a href="javascript:void(0)">PERMITS</a></li>
-			<li><a href="javascript:void(0)">LEGAL</a></li>
-		</ul>
-	</div>
-	<div class="c2">
-		<ul>
-			<li><a href="javascript:void(0)">PHOTOS</a></li>
-		</ul>
-	</div>
-	<div class="c2 last">
-		<ul>
-			<li><a href="javascript:void(0)">CONTACT</a></li>
-		</ul>
-	</div>
-	</div><!-- end footer-nav -->		
+	<div class="footer-nav clear-fix">
+		<div class="c1">			
+			<p class="pseudo-button"><a href="#">GET INVOLVED</a></p>
+			<p class="pseudo-button"><a href="#">NEWSLETTER</a></p>
+			<p class="pseudo-button"><a href="#">CONTRIBUTE</a></p>				
+		</div>
+		<div class="c2">
+			<ul>
+				<li><a href="#">ABOUT</a></li>
+				<li><a href="#">US</a></li>
+				<li><a href="#">PARENT</a></li>
+				<li><a href="#">EDUCATION</a></li>
+				<li><a href="#">OUR EVENTS</a></li>
+				<li><a href="#">OUR PROJECTS</a></li>
+			</ul>
+		</div>
+		<div class="c2">
+			<ul>
+				<li><a href="#">CALENDAR</a></li>
+			</ul>
+		</div>
+		<div class="c2">
+			<ul>
+				<li><a href="#">FORUM</a></li>
+			</ul>
+		</div>
+		<div class="c2">
+			<ul>
+				<li><a href="#">BLOG</a></li>
+				<li><a href="#">EVENTS</a></li>
+				<li><a href="#">PROJECTS</a></li>
+			</ul>
+		</div>
+		<div class="c2">
+			<ul>
+				<li><a href="#">RESOURCES</a></li>
+				<li><a href="#">LAND</a></li>
+				<li><a href="#">PERMITS</a></li>
+				<li><a href="#">LEGAL</a></li>
+			</ul>
+		</div>
+		<div class="c2">
+			<ul>
+				<li><a href="#">PHOTOS</a></li>
+			</ul>
+		</div>
+		<div class="c2 last">
+			<ul>
+				<li><a href="#">CONTACT</a></li>
+			</ul>
+		</div>
+	</div><!-- end footer-nav -->				
 	<p id="copy-right-info" class="delimiter"><?php print ("&copy; " . date ('Y') . " "); ?>Earthship Seattle &bull; All Rights Reserved &bull; <a href="#top"> TOP OF PAGE </a></p>	
 </footer>
-
 </div> <!-- end footer wide -->
-
 <script src="//localhost:35729/livereload.js"></script>
 </body>
 </html>
