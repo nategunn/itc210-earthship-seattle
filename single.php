9bcb101a96ea9196daee68df087438e445270cd0
Nate Gunn <nategunn2000@gmail.com>
2014-11-27 13:46:05 -0800

    remove height on #mid div, clean up single php

--- single.php
+++ single.php
@@ -6,18 +6,20 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <div id="content" class="single">
     <strong class="breadcrumb"><?php if (function_exists('bcn_display')) { bcn_display(); } ?></strong>
-        <h2><?php the_title(); ?></h2>     
+        <h2><?php the_title(); ?></h2>
+        
+    <?php the_content('More &raquo;'); ?>     
     <p class="postmetadata">
     	<span class="date">Posted on <?php the_time('F jS, Y') ?></span> in 
 		<span class="cat"><?php the_category(', ') ?></span>
 		<span class="author"><?php _e('By');?> <?php the_author_posts_link(); ?></span>
     </p>
-    <?php the_content('More &raquo;'); ?>
     
-		<nav class="post-nav">            
-			<div class="alignleft"><?php previous_post_link('&laquo; %link', '%title', true) ?></div>
-			<div class="alignright"><?php next_post_link('%link &raquo;', '%title', true) ?></div>
-		</nav>
+    
+	<nav class="post-nav">            
+		<div class="alignleft"><?php previous_post_link('&laquo; %link', '%title', true) ?></div>
+		<div class="alignright"><?php next_post_link('%link &raquo;', '%title', true) ?></div>
+	</nav>
     
     <?php edit_post_link('Edit this entry.', '<p><small>', '</small></p>'); ?>
 	<?php comments_template(); ?>
