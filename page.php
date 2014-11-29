eb3f2fd57da5f3a66c02ba02889457679a7cd288
tfleischauer <tfleischauer@gmail.com>
2014-11-29 01:22:57 -0800

    margin-top
    margin-top

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 <div id="middle" class="wrapper940">
     
     <div class="breadcrumbs" xmlns:v="http://rdf.data-vocabulary.org/#">
-        // for Breadcrumb NavXT
+        <!-- for Breadcrumb NavXT -->
 	<?php if(function_exists('bcn_display'))
 	{
 	    bcn_display();
