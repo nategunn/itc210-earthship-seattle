283fd268337f57331a63f0de1cc1e4e87bcbd205
Nate Gunn <nategunn2000@gmail.com>
2014-12-01 13:11:52 -0800

    add events-upcoming.php

---
+++ events-upcoming.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: events-upcoming
+*/
+?>
+<?php get_header(); ?>
+
+<div id="middle" class="wrapper940">
+    
+    <div class="breadcrumbs" xmlns:v="http://rdf.data-vocabulary.org/#">
+        <!-- for Breadcrumb NavXT -->
+	<?php if(function_exists('bcn_display'))
+	{
+	    bcn_display();
+	}?>
+    </div>
+
+    <div id="main-content" class="page">
+
+    <?php query_posts( 'cat=events&tag=upcoming' ); ?>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    
+        <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+            <h2><?php the_title(); ?></h2> <!-- ex. "About" -->
+            
+            <?php the_content(); ?>
+            
+        </div>
+
+    <?php endwhile; ?>
+   
+    <?php endif; ?> 
+
+
+</div><!-- #main-content -->
+<div>
+        <?php get_sidebar();?>
+</div>
+<div id='delimiter'></div>
+<?php get_footer();
