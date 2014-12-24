49fdacd51cc8d83e2498e479e4f25368098c61c2
tfleischauer <tfleischauer@gmail.com>
2014-12-24 00:54:34 -0800

    template

--- comments.php
+++ comments.php
@@ -1,18 +1,18 @@
 <?php // Do not delete these lines
 	if ('comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
 		die ('Please do not load this page directly. Thanks!');
-
-        if (!empty($post->post_password)) { // if there's a password
-            if ($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) {  // and it doesn't match the cookie
+	
+	if (!empty($post->post_password)) { // if there's a password
+	    if ($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) {  // and it doesn't match the cookie
 				?>
 				
 				<p class="nocomments">This post is password protected. Enter the password to view comments.<p>
 				
 				<?php
 				return;
-            }
-        }
-
+	    }
+	}
+	
 		/* This variable is for alternating comment background */
 		$oddcomment = 'alt';
 ?>
